620c5b4cc7ec2b32d95af4904b7954d7018b8341
ronidas39 <49780975+ronidas39@users.noreply.github.com>
2020-10-12 17:05:52 +0530

    Update webui.py

--- webui.py
+++ webui.py
@@ -5,7 +5,7 @@ webui=Flask(__name__)
 def create():
     if request.method=="POST":
         if request.form["submit"]=="find_graph":
-            response=requests.get("http://111b9c343af5.ngrok.io/display")
+            response=requests.get("http://825cf6964a7d.ngrok.io/display")
             data=response.json()
             return render_template("results.html",list=data)
         elif request.form["submit"]=="create":
@@ -14,7 +14,7 @@ def create():
             if(name=="" or id==""):
                 return("Please enter the values for employee id or name")
             else:
-             response=requests.get("http://111b9c343af5.ngrok.io/create/"+name+"&"+id)
+             response=requests.get("http://825cf6964a7d.ngrok.io/create/"+name+"&"+id)
              return(response.text)
 
 
